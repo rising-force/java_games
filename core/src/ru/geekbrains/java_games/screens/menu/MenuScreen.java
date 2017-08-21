5adb0032673b090555d6d25549e963c74b85a338
lex <aa@bb.ru>
2017-08-21 20:46:17 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -1,7 +1,45 @@
 package ru.geekbrains.java_games.screens.menu;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import ru.geekuniversity.engine.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
+    private SpriteBatch batch;
+    private Texture img;
+    private Texture textureBackground;
+    private Texture textureCircle;
+
+    @Override
+    public void show() {
+        super.show();
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        textureBackground = new Texture("bg.png");
+        textureCircle = new Texture("circle.png");
+    }
+
+    @Override
+    public void render (float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(textureBackground, 0, 0);
+        batch.draw(img, 0, 0);
+        batch.draw(textureCircle, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+        textureBackground.dispose();
+        textureCircle.dispose();
+        super.dispose();
+    }
 }
