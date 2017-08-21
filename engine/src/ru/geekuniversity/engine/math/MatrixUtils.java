91179f083ccfae04bb892802dcc2deaa27be7cda
lex <aa@bb.ru>
2017-08-21 22:45:00 +0300

    1

--- engine/src/ru/geekuniversity/engine/math/MatrixUtils.java
+++ engine/src/ru/geekuniversity/engine/math/MatrixUtils.java
@@ -2,26 +2,23 @@ package ru.geekuniversity.engine.math;
 
 import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 
 public final class MatrixUtils {
 
     private MatrixUtils() {
-
     }
 
-    public static void method() {
-        Vector2 vector2 = new Vector2(0f, 0f);
-        Matrix3 matrix3 = new Matrix3();
-        matrix3.idt().translate(1f, 1f).scale(2f, 2f);
-        vector2.mul(matrix3);
-        System.out.println(vector2);
+    //Расчёт матрицы перехода 4x4
+    public static void calcTransitionMatrix(Matrix4 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth();
+        float scaleY = dst.getHeight() / src.getHeight();
+        mat.idt().translate(dst.pos.x, dst.pos.y, 0f).scale(scaleX, scaleY, 1f).translate(-src.pos.x, -src.pos.y, 0f);
+    }
 
-        Vector3 vector3 = new Vector3();
-        Matrix4 matrix4 = new Matrix4();
-        matrix4.idt().scale(1f, 2f, 4f).translate(10,3,4);
-        vector3.mul(matrix4);
-
+    //Расчёт матрицы перехода 3x3
+    public static void calcTransitionMatrix(Matrix3 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth();
+        float scaleY = dst.getHeight() / src.getHeight();
+        mat.idt().translate(dst.pos.x, dst.pos.y).scale(scaleX, scaleY).translate(-src.pos.x, -src.pos.y);
     }
 }
