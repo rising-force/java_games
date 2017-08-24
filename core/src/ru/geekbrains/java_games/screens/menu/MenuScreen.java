c7fa225d518124de9a8b1a1c06fd77d45b303f0c
lex <aa@bb.ru>
2017-08-24 18:42:56 +0300

    lesson_2_home_work

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -40,4 +40,52 @@ public class MenuScreen extends Base2DScreen {
         textureCircle.dispose();
         super.dispose();
     }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown: " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("keyUp: " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("keyTyped: " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown: x = " + screenX + " y = " + screenY + " pointer = " + pointer);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp: x = " + screenX + " y = " + screenY + " pointer = " + pointer);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged: x = " + screenX + " y = " + screenY + " pointer = " + pointer);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        System.out.println("mouseMoved: x = " + screenX + " y = " + screenY );
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        System.out.println("scrolled: " + amount);
+        return false;
+    }
 }
