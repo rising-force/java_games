8a3c13bccdf1dad828609c62cc3a31b594abf490
lex <aa@bb.ru>
2017-08-28 22:42:06 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -3,19 +3,17 @@ package ru.geekbrains.java_games.screens.menu;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekuniversity.engine.Base2DScreen;
 import ru.geekuniversity.engine.Sprite2DTexture;
-import ru.geekuniversity.engine.sprites.Sprite;
+import ru.geekuniversity.engine.math.Rect;
 
 public class MenuScreen extends Base2DScreen {
 
-    private Sprite2DTexture textureCircle;
-    private TextureRegion textureRegion;
-    private Sprite circle;
+    private Sprite2DTexture textureBackground;
+    private Background background;
 
     public MenuScreen(Game game) {
         super(game);
@@ -24,9 +22,13 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        textureCircle = new Sprite2DTexture("circle.png");
-        circle = new Sprite(new TextureRegion(textureCircle));
-        circle.setWidthProportion(0.67f);
+        textureBackground = new Sprite2DTexture("textures/bg.png");
+        background = new Background(new TextureRegion(textureBackground));
+    }
+
+    @Override
+    protected void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
@@ -39,13 +41,13 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClearColor(0.7f, 0.7f, 0.7f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        circle.draw(batch);
+        background.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose () {
-        textureCircle.dispose();
+        textureBackground.dispose();
         super.dispose();
     }
 }
