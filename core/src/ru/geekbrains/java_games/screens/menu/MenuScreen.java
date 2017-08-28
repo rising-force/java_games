d4ddb69e815a645119e7341c04960391824975ae
lex <aa@bb.ru>
2017-08-28 21:52:18 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -4,14 +4,17 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekuniversity.engine.Base2DScreen;
+import ru.geekuniversity.engine.sprites.Sprite;
 
 public class MenuScreen extends Base2DScreen {
 
     private Texture textureCircle;
+    private TextureRegion textureRegion;
+    private Sprite circle;
 
     public MenuScreen(Game game) {
         super(game);
@@ -21,6 +24,8 @@ public class MenuScreen extends Base2DScreen {
     public void show() {
         super.show();
         textureCircle = new Texture("circle.png");
+        circle = new Sprite(new TextureRegion(textureCircle));
+        circle.setSize(1f, 1f);
     }
 
     @Override
@@ -33,7 +38,7 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClearColor(0.7f, 0.7f, 0.7f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(textureCircle, -0.5f, -0.5f, 1f, 1f);
+        circle.draw(batch);
         batch.end();
     }
 
---
+++ engine/src/ru/geekuniversity/engine/sprites/Sprite.java
@@ -0,0 +1,71 @@
+package ru.geekuniversity.engine.sprites;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekuniversity.engine.math.Rect;
+
+public class Sprite extends Rect {
+
+    protected float angle;
+    protected float scale = 1f;
+    protected TextureRegion[] regions;
+    protected int frame;
+
+    public Sprite(TextureRegion region) {
+        if(region == null) throw new RuntimeException("Create Sprite with null region");
+        regions = new TextureRegion[1];
+        regions[0] = region;
+    }
+
+    public void resize(Rect worldBounds) {
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return false;
+    }
+
+    public boolean touchMove(Vector2 touch, int pointer) {
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer) {
+        return false;
+    }
+
+    public void update(float deltaTime) {
+
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(
+                regions[frame],
+                getLeft(), getBottom(),
+                halfWidth, halfHeight,
+                getWidth(), getHeight(),
+                scale, scale, angle
+        );
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    @Override
+    public String toString() {
+        return "Sprite: " + " angle = " + angle + " scale = " + scale + " " + super.toString();
+    }
+}
