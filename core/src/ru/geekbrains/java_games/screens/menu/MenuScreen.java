7145b168e89b1de5777df2def5541620c517b30c
lex <aa@bb.ru>
2017-08-28 22:55:45 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -3,17 +3,24 @@ package ru.geekbrains.java_games.screens.menu;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
+import ru.geekbrains.java_games.screens.stars.Star;
 import ru.geekuniversity.engine.Base2DScreen;
 import ru.geekuniversity.engine.Sprite2DTexture;
 import ru.geekuniversity.engine.math.Rect;
+import ru.geekuniversity.engine.math.Rnd;
 
 public class MenuScreen extends Base2DScreen {
 
+    private static final float STAR_WIDTH = 0.01f;
+
     private Sprite2DTexture textureBackground;
+    private TextureAtlas atlas;
     private Background background;
+    private Star star;
 
     public MenuScreen(Game game) {
         super(game);
@@ -23,12 +30,19 @@ public class MenuScreen extends Base2DScreen {
     public void show() {
         super.show();
         textureBackground = new Sprite2DTexture("textures/bg.png");
+        atlas = new TextureAtlas("textures/mainAtlas.pack");
         background = new Background(new TextureRegion(textureBackground));
+        TextureRegion regionStar = atlas.findRegion("star");
+        float vx = Rnd.nextFloat(-0.005f, 0.005f);
+        float vy = Rnd.nextFloat(-0.05f, -0.1f);
+        float starWidth = STAR_WIDTH * Rnd.nextFloat(0.75f, 1f);
+        star = new Star(regionStar, vx, vy, starWidth);
     }
 
     @Override
     protected void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        star.resize(worldBounds);
     }
 
     @Override
@@ -42,12 +56,14 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        star.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose () {
         textureBackground.dispose();
+        atlas.dispose();
         super.dispose();
     }
 }
--- desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
@@ -8,6 +8,11 @@ import ru.geekbrains.java_games.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		float aspect = 9f / 16f;
+        float aspect = 3f / 4f;
+
+		config.width = 450;
+		config.height = (int)(config.width / aspect);
 		new LwjglApplication(new StarGame(), config);
 	}
 }
