1f4fc9fdabbb445a138325d951d1c3154d13a8b8
lex <aa@bb.ru>
2017-09-04 21:38:40 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/game/MainShip.java
+++ core/src/ru/geekbrains/java_games/screens/game/MainShip.java
@@ -1,4 +1,15 @@
 package ru.geekbrains.java_games.screens.game;
 
-public class MainShip {
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.geekuniversity.engine.sprites.Sprite;
+
+public class MainShip extends Sprite {
+
+    public MainShip(TextureAtlas atlas) {
+        TextureRegion region = atlas.findRegion("main_ship");
+        region.split()
+//        super(region);
+    }
 }
