4e48732e400664820e3fe14201489096c8941c4d
lex <aa@bb.ru>
2017-09-04 20:46:08 +0300

    1

--- desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import ru.geekbrains.java_games.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		float aspect = 480f / 854f;
-//        float aspect = 3f / 4f;
+//		float aspect = 480f / 854f;
+        float aspect = 3f / 4f;
 
 		config.height = 800;
 		config.width = (int)(config.height * aspect);
