504dfc67b05ee88ce832494d6592e0a6af5bb630
lex <aa@bb.ru>
2017-09-11 20:15:31 +0300

    1

--- core/src/ru/geekbrains/java_games/screens/stars/Star.java
+++ core/src/ru/geekbrains/java_games/screens/stars/Star.java
@@ -12,8 +12,8 @@ public class Star extends Sprite {
     final Vector2 v = new Vector2();
     private Rect worldBounds;
 
-    public Star(TextureRegion textureRegion, float vx, float vy, float height) {
-        super(textureRegion);
+    public Star(TextureRegion region, float vx, float vy, float height) {
+        super(region);
         v.set(vx, vy);
         setHeightProportion(height);
     }
